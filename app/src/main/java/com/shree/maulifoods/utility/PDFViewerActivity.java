c83bb8f5ba16144fd26c6286a48e76d26dd6775f
admin <angular4shar@gmail.com>
2022-04-09 19:28:35 +0530

    update to git

--- app/src/main/java/com/shree/maulifoods/utility/PDFViewerActivity.java
+++ app/src/main/java/com/shree/maulifoods/utility/PDFViewerActivity.java
@@ -1,27 +1,38 @@
 package com.shree.maulifoods.utility;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.os.Environment.DIRECTORY_DOCUMENTS;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.shree.maulifoods.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class PDFViewerActivity extends AppCompatActivity {
 
@@ -35,6 +46,12 @@ public class PDFViewerActivity extends AppCompatActivity {
     int downloadedSize = 0, totalsize;
     String download_file_url = "https://docs.google.com/viewerng/viewer?embedded=true&url=https://kalapms.com/media/receipt.pdf", TAG = "***PDFViewerActivity***";
     float per = 0;
+
+    int pageHeight = 1120;
+    int pagewidth = 792;
+    Bitmap bmp, scaledbmp;
+    // constant code for runtime permissions
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +69,7 @@ public class PDFViewerActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(0.0f);
         getSupportActionBar().show();
 
-        downloadAndOpenPDF();
+        //downloadAndOpenPDF();
 
       /*  Uri path = Uri.parse(download_file_url);
         Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -85,111 +102,186 @@ public class PDFViewerActivity extends AppCompatActivity {
         webSettings.setPluginState(WebSettings.PluginState.ON);
         webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
         webSettings.setBuiltInZoomControls(true);*/
+
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.klogo);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
+
+        // below code is used for
+        // checking our permissions.
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+        generatePDF();
+        //createandDisplayPdf();
     }
 
-    void downloadAndOpenPDF() {
-        new Thread(new Runnable() {
-            public void run() {
+    public void createandDisplayPdf() {
 
-                Uri path = Uri.fromFile(downloadFile(download_file_url));
-                try {
-                    /*Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setDataAndType(path, "application/pdf");
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                    finish();*/
-                } catch (ActivityNotFoundException e) {
-                    Log.d(TAG, "PDF Reader application is not installed in your device");
+        try {
+            String stringFilePath = Environment.getExternalStorageDirectory().getPath() + "/Download/ProgrammerWorld.pdf";
+            File file = new File(stringFilePath);
+
+            PdfDocument pdfDocument = new PdfDocument();
+            PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
+            PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+            Paint paint = new Paint();
+
+            int x = 10, y = 25;
+
+            for (String line : "Audumbar Gavali".split("\n")) {
+                page.getCanvas().drawText(line, x, y, paint);
+
+                y += paint.descent() - paint.ascent();
+            }
+            pdfDocument.finishPage(page);
+            try {
+                pdfDocument.writeTo(new FileOutputStream(file));
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "ioException:" + e);
+            }
+            pdfDocument.close();
+
+        } catch (Exception e) {
+            Log.d(TAG, "ioException:" + e);
+        }
+
+        //viewPdf("newFile.pdf", "Dir");
+    }
+
+    // Method for opening a pdf file
+    private void viewPdf(String file, String directory) {
+
+        File pdfFile = new File(Environment.getExternalStorageDirectory() + "/" + directory + "/" + file);
+        Uri path = Uri.fromFile(pdfFile);
+
+        // Setting the intent for pdf reader
+        Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+        pdfIntent.setDataAndType(path, "application/pdf");
+        pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        try {
+            startActivity(pdfIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(PDFViewerActivity.this, "Can't read pdf file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void generatePDF() {
+
+        PdfDocument pdfDocument = new PdfDocument();
+
+        Paint paint = new Paint();
+        Paint title = new Paint();
+
+        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
+        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+        Canvas canvas = myPage.getCanvas();
+        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(15);
+        title.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        canvas.drawText("A portal for IT professionals.", 209, 100, title);
+        canvas.drawText("Geeks for Geeks", 209, 80, title);
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        title.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        title.setTextSize(15);
+        title.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText("This is sample document which we have created.", 396, 560, title);
+
+        pdfDocument.finishPage(myPage);
+        String fileName = "Receipt.pdf";
+
+        File dir = null;
+       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d(TAG, "ok1");
+            dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        } else {
+            Log.d(TAG, "ok2");
+            dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        }*/
+
+        if (Build.VERSION_CODES.R > Build.VERSION.SDK_INT) {
+            dir = new File(Environment.getExternalStorageDirectory().getPath()+ "//KALA");
+        } else {
+            dir = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).getPath()+ "//KALA");
+        }
+
+        if (dir.exists()) {
+            for (File tempFile : dir.listFiles()) {
+                tempFile.delete();
+            }
+        }
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        //File file = new File(dir, fileName);
+        File file = new File(dir, fileName);
+        Log.d(TAG, "filePath1 " + dir);
+
+        try {
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Toast.makeText(PDFViewerActivity.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+            openPdf(dir + "/"+fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            pdfDocument.close();
+        }
+    }
+
+    private void openPdf(String filePath) {
+
+        File file = new File(filePath);
+        if (file.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(file);
+            intent.setDataAndType(uri, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(PDFViewerActivity.this, "No Application available to view pdf", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denined.", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
             }
-        }).start();
-
-    }
-
-    File downloadFile(String dwnload_file_path) {
-        File file = null;
-        try {
-
-            URL url = new URL(dwnload_file_path);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setDoOutput(true);
-
-            // connect
-            urlConnection.connect();
-
-            // set the path where we want to save the file
-            File SDCardRoot = Environment.getExternalStorageDirectory();
-            // create a new file, to save the downloaded file
-            file = new File(SDCardRoot, dest_file_path);
-
-            FileOutputStream fileOutput = new FileOutputStream(file);
-            // Stream used for reading the data from the internet
-            InputStream inputStream = urlConnection.getInputStream();
-
-            // this is the total size of the file which we are
-            // downloading
-            totalsize = urlConnection.getContentLength();
-            setText("Starting PDF download...");
-
-            // create a buffer...
-            byte[] buffer = new byte[1024 * 1024];
-            int bufferLength = 0;
-
-            while ((bufferLength = inputStream.read(buffer)) > 0) {
-                fileOutput.write(buffer, 0, bufferLength);
-                downloadedSize += bufferLength;
-                per = ((float) downloadedSize / totalsize) * 100;
-                setText("Total PDF File size  : "
-                        + (totalsize / 1024)
-                        + " KB\n\nDownloading PDF " + (int) per
-                        + "% complete");
-            }
-            // close the output stream when complete //
-            fileOutput.close();
-            setText("Download Complete. Open PDF Application installed in the device.");
-
-        } catch (final MalformedURLException e) {
-            setTextError("Some error occured. Press back and try again.", Color.RED);
-        } catch (final IOException e) {
-            setTextError("Some error occured. Press back and try again.", Color.RED);
-        } catch (final Exception e) {
-            setTextError("Failed to download image. Please check your internet connection.", Color.RED);
-        }
-        return file;
-    }
-
-    void setTextError(final String message, final int color) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                Log.d(TAG, message);
-            }
-        });
-
-    }
-
-    void setText(final String txt) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                Log.d(TAG, " Error txt " + txt);
-            }
-        });
-
-    }
-
-    class WebView1 extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            // TODO Auto-generated method stub
-            super.onPageFinished(view, url);
-            progressInfo.ProgressHide();
         }
     }
 
